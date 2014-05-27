738b18be51457f10eba4008ffd5bf40de697c1d2
Joey Hiller <jthiller@gmail.com>
2014-05-27 16:08:55 -0700

    Added TipViewController

--- Tip Calculator/AppDelegate.m
+++ Tip Calculator/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TipViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,10 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    TipViewController *vc = [[TipViewController alloc] init];
+    self.window.rootViewController = vc;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Tip Calculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  Tip Calculator
+//
+//  Created by Joey Hiller on 5/27/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
