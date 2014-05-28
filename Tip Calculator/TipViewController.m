24607960daead2d0c969277fe9a9a9d7272e53f4
Joey Hiller <jthiller@gmail.com>
2014-05-28 13:55:37 -0700

    Math for bill split working

--- Tip Calculator/TipViewController.m
+++ Tip Calculator/TipViewController.m
@@ -10,12 +10,14 @@
 
 @interface TipViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *billTextFieldLabel;
 @property (weak, nonatomic) IBOutlet UITextField *billTextField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 @property (weak, nonatomic) IBOutlet UIStepper *splitAdd;
 @property (weak, nonatomic) IBOutlet UILabel *splitCount;
+@property (weak, nonatomic) IBOutlet UILabel *splitTotalLabel;
 
 
 - (IBAction)onTap:(id)sender;
@@ -28,7 +30,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        self.title = @"Tip Calculator";
+        //self.title = @"Tip Calculator";
     }
     return self;
 }
@@ -53,12 +55,18 @@
 -(void)updateValues {
     float billAmount = [self.billTextField.text floatValue];
     
+    int peopleCount = [self.splitAdd value];
+
+    
     NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
     float tipAmount = billAmount * [tipValues [self.tipControl.selectedSegmentIndex] floatValue];
     float totalAmount = tipAmount + billAmount;
+    float splitAmount = totalAmount / peopleCount;
     
     self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
     self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+    self.splitCount.text = [NSString stringWithFormat:@"%i people",peopleCount];
+    self.splitTotalLabel.text = [NSString stringWithFormat:@"$%0.2f", splitAmount];
 }
 
 @end
