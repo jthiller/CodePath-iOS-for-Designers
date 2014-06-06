d6e29fcc08a228bd0fdeec31a79d62c6e2835b6d
Joey Hiller <jthiller@gmail.com>
2014-06-05 18:16:15 -0700

    folder moving

---
+++ Project 1 - Facebook View/CodePath_Week1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CodePath_Week1
+//
+//  Created by Joey Hiller on 6/5/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project 1 - Facebook View/CodePath_Week1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CodePath_Week1
+//
+//  Created by Joey Hiller on 6/5/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
