ecc92fc678ec94ed4eb0480d526c1e1458de7c7f
Joey Hiller <jthiller@gmail.com>
2014-06-10 16:40:28 -0700

    Install Cocoapods and TTTAttributedLabel

---
+++ Project 1 - Facebook View/Pods/Pods-CodePath-Week1-TTTAttributedLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodePath_Week1_TTTAttributedLabel : NSObject
+@end
+@implementation PodsDummy_Pods_CodePath_Week1_TTTAttributedLabel
+@end
---
+++ Project 1 - Facebook View/Pods/Pods-CodePath-Week1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodePath_Week1 : NSObject
+@end
+@implementation PodsDummy_Pods_CodePath_Week1
+@end
