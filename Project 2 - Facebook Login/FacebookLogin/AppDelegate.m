a0f49c75510e3a43f2e3f7dba1ece9fae9251727
Joey Hiller <jthiller@gmail.com>
2014-06-11 23:37:24 -0700

    Added LoginViewController

--- Project 2 - Facebook Login/FacebookLogin/AppDelegate.m
+++ Project 2 - Facebook Login/FacebookLogin/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,11 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    LoginViewController *loginViewController = [[LoginViewController alloc] init];
+    self.window.rootViewController = loginViewController;
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Project 2 - Facebook Login/FacebookLogin/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  FacebookLogin
+//
+//  Created by Joey Hiller on 6/11/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
