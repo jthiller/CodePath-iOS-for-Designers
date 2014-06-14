ac741bc098d9697a4b38d91faf4b6d8bd297d25b
Joey Hiller <jthiller@gmail.com>
2014-06-14 14:04:52 -0700

    View reacts to keyboard initialization

--- Project 2 - Facebook Login/FacebookLogin/LoginViewController.m
+++ Project 2 - Facebook Login/FacebookLogin/LoginViewController.m
@@ -9,6 +9,17 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()
+// Touch gesture controller
+- (IBAction)onTap:(id)sender;
+
+// Buttons
+- (IBAction)onUsernameEntered:(id)sender;
+- (IBAction)onLoginButton:(id)sender;
+
+// Keyboard stuff
+@property (weak, nonatomic) IBOutlet UIView *loginAreaView;
+- (void)willShowKeyboard:(NSNotification *)notification;
+- (void)willHideKeyboard:(NSNotification *)notification;
 
 @end
 
@@ -19,6 +30,11 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        
+        // Register the methods for the keyboard hide/show events
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willShowKeyboard:) name:UIKeyboardWillShowNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willHideKeyboard:) name:UIKeyboardWillHideNotification object:nil];
+        
     }
     return self;
 }
@@ -35,4 +51,66 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)willShowKeyboard:(NSNotification *)notification {
+    NSDictionary *userInfo = [notification userInfo];
+    
+    // Get the keyboard height and width from the notification
+    // Size varies depending on OS, language, orientation
+    CGSize kbSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    NSLog(@"Height: %f Width: %f", kbSize.height, kbSize.width);
+    
+    // Get the animation duration and curve from the notification
+    NSNumber *durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration = durationValue.doubleValue;
+    NSNumber *curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey];
+    UIViewAnimationCurve animationCurve = curveValue.intValue;
+    
+    // Move the view with the same duration and animation curve so that it will match with the keyboard animation
+    [UIView animateWithDuration:animationDuration
+                          delay:0.0
+                        options:(animationCurve << 16)
+                     animations:^{
+                         self.loginAreaView.frame = CGRectMake(0, self.view.frame.size.height - kbSize.height - self.loginAreaView.frame.size.height, self.loginAreaView.frame.size.width, self.loginAreaView.frame.size.height);
+                     }
+                     completion:nil];
+}
+
+- (void)willHideKeyboard:(NSNotification *)notification {
+    NSDictionary *userInfo = [notification userInfo];
+    
+    // Get the keyboard height and width from the notification
+    // Size varies depending on OS, language, orientation
+    // CGSize kbSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    // NSLog(@"Down - Height: %f Width: %f", kbSize.height, kbSize.width);
+    
+    // Get the animation duration and curve from the notification
+    NSNumber *durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration = durationValue.doubleValue;
+    NSNumber *curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey];
+    UIViewAnimationCurve animationCurve = curveValue.intValue;
+    
+    // Move the view with the same duration and animation curve so that it will match with the keyboard animation
+    [UIView animateWithDuration:animationDuration
+                          delay:0.0
+                        options:(animationCurve << 16)
+                     animations:^{
+                         self.loginAreaView.frame = CGRectMake(0,  // X-axis
+                           // I feel like this shouldn't be hard-coded, but it works...
+                           0, // Y-axis
+                           self.loginAreaView.frame.size.width,
+                           self.loginAreaView.frame.size.height);
+                     }
+                     completion:nil];
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onLoginButton:(id)sender {
+    NSLog(@"'Log In' pressed");
+}
+
+- (IBAction)onUsernameEntered:(id)sender {
+}
 @end
