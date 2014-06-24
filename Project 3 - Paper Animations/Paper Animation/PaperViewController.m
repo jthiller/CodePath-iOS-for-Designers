d4cdb6f70dd2964f43abda5b63284a34ae41b14f
Joey Hiller <jthiller@gmail.com>
2014-06-24 08:20:55 -0700

    progress on transitions

--- Project 3 - Paper Animations/Paper Animation/PaperViewController.m
+++ Project 3 - Paper Animations/Paper Animation/PaperViewController.m
@@ -32,6 +32,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+//    [[UIApplication sharedApplication] setStatusBarHidden:YES];
+    [[UIApplication sharedApplication] setStatusBarHidden:true];
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,26 +54,82 @@
     else if (sender.state == UIGestureRecognizerStateChanged) {
 //        NSLog(@"viewdidPan %f %f", panPosition.x, panPosition.y);
 //        NSLog(@"vel %f", panVelocity.y);
+        
+        self.headlinesView.center = CGPointMake(self.headlinesView.center.x, (panPosition.y - self.headlinesTouchOffset.y));
 
         
-//        [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:.3 initialSpringVelocity:15 options:0 animations:^{
-            self.headlinesView.center = CGPointMake(self.headlinesView.center.x, (panPosition.y - self.headlinesTouchOffset.y));
-//        } completion:^(BOOL finished) {
-//            nil;
-//        }];
+//        if ((panPosition.y - self.headlinesTouchOffset.y) >= self.view.frame.size.height-50) {
+//            self.headlinesView.center = CGPointMake(self.headlinesView.center.x, (panPosition.y - self.headlinesTouchOffset.y));
+//            NSLog(@"viewdidPanScaled %f", panPosition.y - self.headlinesTouchOffset.y);
+//            NSLog(@"%f",self.view.frame.size.height-50);
+//        }
+//        else if ((panPosition.y - self.headlinesTouchOffset.y) < self.view.frame.size.height-50){
+//            self.headlinesView.center = CGPointMake(self.headlinesView.center.x, (panPosition.y - self.headlinesTouchOffset.y));
+//            NSLog(@"viewdidPan %f", panPosition.y - self.headlinesTouchOffset.y);
+//
+//        }
     }
     else if (sender.state == UIGestureRecognizerStateEnded) {
         NSLog(@"Gesture has ended with velocity %f", panVelocity.y);
-        if (panVelocity.y >= 1000) {
+        // Swipe down, continue to bottom
+        if (panVelocity.y >= 500) {
             [UIView animateWithDuration:(panVelocity.y/10000) animations:^{
                 self.headlinesView.center = CGPointMake(self.headlinesView.center.x, 800);
             } completion:nil];
         }
-        else if (panVelocity.y <= -1000) {
+        // Swipe up, Return to center
+        else if (panVelocity.y <= -500) {
             [UIView animateWithDuration:(panVelocity.y/-10000) animations:^{
                 self.headlinesView.center = CGPointMake(self.headlinesView.center.x, self.view.center.y);
             } completion:nil];
         }
+        
+        else if (self.headlinesView.center.y <= self.view.center.y+200) {
+            [UIView animateWithDuration:(.25) animations:^{
+                self.headlinesView.center = CGPointMake(self.headlinesView.center.x, self.view.center.y);
+            } completion:nil];
+        }
+        else if (self.headlinesView.center.y > self.view.center.y+200) {
+            [UIView animateWithDuration:(.25) animations:^{
+                self.headlinesView.center = CGPointMake(self.headlinesView.center.x, 800);
+            } completion:nil];
+        }
+        
+        // From center, return to center if moved upward.
+        else if (self.headlinesView.center.y < self.view.center.y) {
+            [UIView animateWithDuration:abs((self.headlinesView.center.y - self.view.center.y))/300.0
+                                  delay:0
+                 usingSpringWithDamping:.5
+                  initialSpringVelocity:3
+                                options:0
+                             animations:^{
+                                 self.headlinesView.center = CGPointMake(self.headlinesView.center.x, self.view.center.y);
+                             } completion:nil
+             ];
+        }
+//        //If the top of the headlines view is
+//        else if ((self.headlinesView.center.y-(self.headlinesView.frame.size.height/2)) > self.view.frame.origin.y + 100 ) {
+//            [UIView animateWithDuration:(.5) animations:^{
+//                self.headlinesView.center = CGPointMake(self.headlinesView.center.x, self.view.center.y);
+//                NSLog(@"otheWAY");
+//            } completion:nil];
+//        }
+        
+        
+//        if (self.headlinesView.center.y < self.view.center.y) {
+//            [UIView animateWithDuration:.5
+//                                  delay:0
+//                 usingSpringWithDamping:.15
+//                  initialSpringVelocity:1
+//                                options:0
+//                             animations:^{
+//                                 self.headlinesView.center = CGPointMake(self.headlinesView.center.x, self.view.center.y);
+//                             } completion:nil
+//             ];
+////            [UIView animateWithDuration:(.25) animations:^{
+////                self.headlinesView.center = CGPointMake(self.headlinesView.center.x, self.view.center.y);
+////            } completion:nil];
+//        }
     }
     
 }
--- Project 3 - Paper Animations/README.md
+++ Project 3 - Paper Animations/README.md
@@ -7,8 +7,8 @@ Homework 3
 * **Which required and optional stories have you completed?**
     * The homework this week is to make an interactive mock of the Paper navigation. Read the required and optional items below.
       * :white_check_mark: Dragging the headline should reveal the menu
-      * :white_large_square: If you're dragging down, releasing the headline should animate the headline to the down position
-      * :white_large_square: If you're dragging up, releasing the headline should animate the headline to the up position
+      * :white_check_mark: If you're dragging down, releasing the headline should animate the headline to the down position
+      * :white_check_mark: If you're dragging up, releasing the headline should animate the headline to the up position
       * :white_large_square: If you're dragging the headline up past the top, the friction should increase
       * :white_large_square: Optional: Fade in different headlines every few seconds
       * :white_large_square: Optional: Dragging on the scrollable feed of headlines should change the scale of the feed
