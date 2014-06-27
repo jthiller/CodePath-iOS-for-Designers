775972dd6e3f2c6b729bfba8298dd6ce87846709
Joey Hiller <jthiller@gmail.com>
2014-06-27 16:56:48 -0700

    Week 4 project init

---
+++ Project 4 - Canvas/Project 4 - Canvas/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project 4 - Canvas
+//
+//  Created by Joey Hiller on 6/27/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project 4 - Canvas/Project 4 - Canvas/CanvasViewController.h
@@ -0,0 +1,13 @@
+//
+//  CanvasViewController.h
+//  Project 4 - Canvas
+//
+//  Created by Joey Hiller on 6/27/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CanvasViewController : UIViewController
+
+@end
