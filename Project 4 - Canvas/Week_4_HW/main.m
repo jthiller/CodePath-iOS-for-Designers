0a68b1881ff2b64afbb7e42fae6551e03f405545
Joey Hiller <jthiller@gmail.com>
2014-06-30 22:19:33 -0700

    Remember rotation/scale state

---
+++ Project 4 - Canvas/Week_4_HW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Week_4_HW
+//
+//  Created by Joey Hilller on 6/29/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Project 4 - Canvas/Week_4_HWTests/Week_4_HWTests.m
@@ -0,0 +1,34 @@
+//
+//  Week_4_HWTests.m
+//  Week_4_HWTests
+//
+//  Created by Kristina Frost on 6/28/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Week_4_HWTests : XCTestCase
+
+@end
+
+@implementation Week_4_HWTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
