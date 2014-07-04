03b30f1a0d3c3917bcec5f4f46228374ec6c3c8d
Joey Hiller <jthiller@gmail.com>
2014-07-04 15:31:06 -0700

    Init project

---
+++ Project 5 - TumblrApp/TumblrApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TumblrApp
+//
+//  Created by Joey Hiller on 7/4/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project 5 - TumblrApp/TumblrApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TumblrApp
+//
+//  Created by Joey Hiller on 7/4/14.
+//  Copyright (c) 2014 Joey Hiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
